35501e29e938ea3074cc97aa4eb1728eca850555
David Johansson <david.n.johansson@live.com>
2023-11-19 11:40:00 +0100

    ..

--- src/app/services/track.service.ts
+++ src/app/services/track.service.ts
@@ -1,6 +1,5 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Track } from '../interfaces/track.interface';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class TrackService {
@@ -10,53 +9,22 @@ export class TrackService {
       id: 1,
       artist: 'Kevin MacLeod',
       name: 'Airport Lounge',
-      gradient: this.getGradient(),
+      gradient: `linear-gradient(to bottom right, #F43DB3, #FF9D5C)`,
       url: 'assets/tracks/Airport Lounge.mp3',
     },
     {
       id: 2,
       artist: 'Kevin MacLeod',
       name: 'Awesome Call',
-      gradient: this.getGradient(),
+      gradient: `linear-gradient(to bottom right, #06E3B7, #00B6F5)`,
       url: 'assets/tracks/Awesome Call.mp3',
     },
     {
       id: 3,
       artist: 'Kevin MacLeod',
       name: 'Backed Vibes',
-      gradient: this.getGradient(),
+      gradient: `linear-gradient(to bottom right, #FB3594, #6102FE)`,
       url: 'assets/tracks/Backed Vibes.mp3',
     },
   ];
-
-  private http = inject(HttpClient);
-
-  public load() {
-    
-  }
-
-  private getGradient() {
-    const colors = this.getRandomColors();
-    return `linear-gradient(to bottom right, ${colors[0]}, ${colors[1]})`;
-  }
-
-  private getRandomColors() {
-    let components = [0, 0, 0];
-    const highIndex = Math.floor(Math.random() * 3);
-    components[highIndex] = Math.floor(Math.random() * 55 + 200);
-
-    for (let i = 0; i < 3; i++) {
-      if (i !== highIndex) {
-        components[i] = Math.floor(Math.random() * 201);
-      }
-    }
-
-    const firstColor = `#${components.map(c => c.toString(16).padStart(2, '0')).join('')}`;
-
-    // Make the second color brighter and lighter based on the first color
-    const brighterComponents = components.map(c => Math.min(c + Math.floor(Math.random() * 55), 255));
-    const secondColor = `#${brighterComponents.map(c => c.toString(16).padStart(2, '0')).join('')}`;
-
-    return [firstColor, secondColor];
-  }
 }
