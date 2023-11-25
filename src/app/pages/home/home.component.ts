50bb44b0df34e165594dcf2d4b9ba0a77c4326f3
David Johansson <david.n.johansson@live.com>
2023-11-25 10:38:44 +0100

    ..

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,19 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrackService } from '../../services/track.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
+import { CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Track } from '../../interfaces/track.interface';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    CdkDrag,
+    CdkDropList,
     CommonModule,
     MatButtonModule,
     MatIconModule,
@@ -22,4 +26,9 @@ import { RouterModule } from '@angular/router';
 })
 export class HomeComponent {
   public trackService = inject(TrackService);
+  private cdr = inject(ChangeDetectorRef);
+
+  drop(event: CdkDragDrop<Track[]>) {
+    moveItemInArray(this.trackService.tracks, event.previousIndex, event.currentIndex);
+  }
 }
