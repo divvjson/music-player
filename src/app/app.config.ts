c49c998cb3bd053294543cdf99a99f2acd8c03e6
David Johansson <david.n.johansson@live.com>
2023-12-03 20:42:34 +0100

    hash location strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideHttpClient(withFetch()),
     provideRouter(routes),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    }
   ]
 };
